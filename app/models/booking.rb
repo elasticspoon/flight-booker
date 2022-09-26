1219545a7db033167bcfb330f4d5b8b03ba702ee
Yuri Bocharov <quesadillaman@gmail.com>
2022-09-26 10:37:24 -0400

    inital booking model

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,3 +1,5 @@
 class Booking < ApplicationRecord
-  belongs_to :flight
+  belongs_to :flight, inverse_of: :bookings, dependent: false
+  has_many :passengers, dependent: :destroy, inverse_of: :booking
+  accepts_nested_attributes_for :passengers, allow_destroy: true, limit: 4
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Booking, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    subject { build(:booking) }
+    it { should belong_to(:flight).inverse_of(:bookings).dependent(false) }
+    it { should have_many(:passengers).dependent(:destroy).inverse_of(:booking) }
+    it { should accept_nested_attributes_for(:passengers).allow_destroy(true).limit(4) }
+  end
 end
