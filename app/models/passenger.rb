a8c18d896930994a627a1a7db0767631a7b0e6e0
Yuri Bocharov <quesadillaman@gmail.com>
2022-09-26 10:41:57 -0400

    basic passenger model set up

--- app/models/passenger.rb
+++ app/models/passenger.rb
@@ -1,3 +1,7 @@
 class Passenger < ApplicationRecord
-  belongs_to :booking
+  belongs_to :booking, inverse_of: :passengers, dependent: false
+  has_one :flight, through: :booking
+
+  validates :name, presence: true
+  validates :email, presence: true
 end
--- spec/models/passenger_spec.rb
+++ spec/models/passenger_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Passenger, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    subject { build(:passenger) }
+    it { should belong_to(:booking).inverse_of(:passengers).dependent(false) }
+    it { should have_one(:flight).through(:booking) }
+  end
+
+  describe 'validations' do
+    subject { build(:passenger) }
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:email) }
+  end
 end
