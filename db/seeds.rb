fadc0896d85b564e1423d952cd9dbfe71472d0ed
Yuri Bocharov <quesadillaman@gmail.com>
2022-09-26 13:43:15 -0400

    airport and flight seeding added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+
+Airport.delete_all
+30.times do
+  Airport.create(
+    location: "#{Faker::Address.city}, #{Faker::Address.country}",
+    code: Faker::Address.unique.country_code_long
+  )
+end
+50.times do
+  arrival_airport, departure_airport = Airport.all.sample(2)
+  Flight.create(
+    arrival_airport:,
+    departure_airport:,
+    departure: Faker::Time.forward(days: 30, period: :morning),
+    duration: Faker::Number.between(from: 3600, to: 86_400)
+  )
+end
