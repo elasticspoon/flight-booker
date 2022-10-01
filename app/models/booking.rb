f3ee50ccfd81640f7252a0d6dc9800df3490dd69
Yuri Bocharov <quesadillaman@gmail.com>
2022-10-01 18:39:49 -0400

    validations for number of passengers

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,16 @@ class Booking < ApplicationRecord
   belongs_to :flight, inverse_of: :bookings, dependent: false
   has_many :passengers, dependent: :destroy, inverse_of: :booking
   accepts_nested_attributes_for :passengers, allow_destroy: true, limit: 4
+
+  validates :passengers, presence: true
+
+  validate :passenger_count
+
+  private
+
+  def passenger_count
+    return if passengers.size.between?(1, 4)
+
+    errors.add(:passengers, 'must be between 1 and 4')
+  end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -7,4 +7,25 @@ RSpec.describe Booking, type: :model do
     it { should have_many(:passengers).dependent(:destroy).inverse_of(:booking) }
     it { should accept_nested_attributes_for(:passengers).allow_destroy(true).limit(4) }
   end
+
+  describe 'validations' do
+    let(:booking) { build(:booking) }
+    subject { booking }
+    it { should validate_presence_of(:passengers) }
+
+    it 'should not allow more than 4 passengers' do
+      booking = build_booking_with_passengers(passenger_count: 5)
+      expect(booking).not_to be_valid
+    end
+
+    it 'should not allow less than 1 passenger' do
+      booking = build_booking_with_passengers(passenger_count: 0)
+      expect(booking).not_to be_valid
+    end
+
+    it 'should allow passengers' do
+      booking = build_booking_with_passengers(passenger_count: 2)
+      expect(booking).to be_valid
+    end
+  end
 end
