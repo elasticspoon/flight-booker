7597df034d38648d72c52e6cd2f6e29b7c05be5c
Yuri Bocharov <quesadillaman@gmail.com>
2022-10-01 18:39:23 -0400

    additional booking building methods

--- spec/factories/bookings.rb
+++ spec/factories/bookings.rb
@@ -3,3 +3,15 @@ FactoryBot.define do
     flight
   end
 end
+
+def create_booking_with_passengers(passenger_count: 2)
+  FactoryBot.create(:booking) do |booking|
+    FactoryBot.create_list(:passenger, passenger_count, booking:)
+  end
+end
+
+def build_booking_with_passengers(passenger_count: 2)
+  FactoryBot.build(:booking) do |booking|
+    FactoryBot.build_list(:passenger, passenger_count, booking:)
+  end
+end
